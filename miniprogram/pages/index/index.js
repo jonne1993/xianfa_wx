95d42747e92201aedecefaf98802e508853ceb0a
fengkai <whhitfk@163.com>
2018-11-12 03:26:57 +0800

    xw测试---
    xw测试---xw测试---xw测试---

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -57,7 +57,7 @@ Page({
         wx.navigateTo({
           url: '../userConsole/userConsole',
         })
-        console.log('app.globalData.tonkenid--徐伟个222货')
+        console.log('app.globalData.tonkenid--徐伟个222--3333333333货')
         wx.request({
           url: app.globalData.ApiUrl + 'w/testapi', //仅为示例，并非真实的接口地址
           data: {
