f4e3b0e8945efe6a5515c9e1690f80614a5732df
jonne1993 <657246459@qq.com>
2018-11-12 10:38:47 +0800

    what the fuck

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -57,7 +57,7 @@ Page({
         wx.navigateTo({
           url: '../userConsole/userConsole',
         })
-        console.log('00000000000000000000022222222220000')
+        console.log('what the fuck.......')
         wx.request({
           url: app.globalData.ApiUrl + 'w/testapi', //仅为示例，并非真实的接口地址
           data: {
